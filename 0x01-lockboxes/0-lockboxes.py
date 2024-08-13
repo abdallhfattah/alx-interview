a894a913ed3fadac436c417edb2c111760c46937
abdallhfattah <abdallh.fattah33@gmail.com>
2024-08-13 21:04:50 +0300

    locked boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+  unlocked = [False] * len(boxes)
+  unlocked[0] = True
+  for box in boxes:
+    for key in box:
+      unlocked[key] = True
+  return all(unlocked)
+  
+  
