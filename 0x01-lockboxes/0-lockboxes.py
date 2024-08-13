5bd22c76b2e3d433940ada97992c73593b990b01
abdallhfattah <abdallh.fattah33@gmail.com>
2024-08-13 21:15:00 +0300

    locked boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,19 +2,25 @@
 """
 Solution to lockboxes problem
 """
+
+
 def canUnlockAll(boxes):
-  """
-  Determines whether a series of locked boxes can be opened
-  based on keys that can be attained.
-  Solution to the lockboxes problem
-  """
+    """
+    Determines whether a series of locked boxes can be opened
+    based on keys that can be attained.
+    Solution to the lockboxes problem
+    """
+    if (type(boxes)) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
 
-  if(type(boxes) is not list or len(boxes) == 0):
-    return False
-
-  unlocked = [False] * len(boxes)
-  unlocked[0] = True
-  for box in boxes:
-    for key in box:
-      unlocked[key] = True
-  return all(unlocked)
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
