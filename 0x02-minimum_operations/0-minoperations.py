63fcac1de7b54b2f99b808007b3280350b1cc2e1
abdallhfattah <abdallh.fattah33@gmail.com>
2024-08-31 17:36:49 +0300

    all

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+    """Compute the minimum number of operations
+    to reach exactly n 'H' characters."""
+    memo = {}
+
+    def dp(count, copied):
+        if count == n:
+            return 0
+        if count > n:
+            return float("inf")
+
+        if (count, copied) in memo:
+            return memo[(count, copied)]
+
+        res = float("inf")
+
+        res = min(1 + dp(count + copied, copied), 2 + dp(count + count, count))
+        memo[(count, copied)] = res
+        return res
+
+    return 1 + dp(1, 1)
