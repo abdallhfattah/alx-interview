0c86d9278d59eb8c10784cb4f8b0a19c8b82dbe2
abdallhfattah <abdallh.fattah33@gmail.com>
2024-08-31 18:33:08 +0300

    min operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,11 +4,11 @@
 
 
 def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters """
-    next = 'H'
-    body = 'H'
+    """Minimum Operations needed to get n H characters"""
+    next = "H"
+    body = "H"
     op = 0
-    while (len(body) < n):
+    while len(body) < n:
         if n % len(body) == 0:
             op += 2
             next = body
