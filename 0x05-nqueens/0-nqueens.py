0d5902ba997457a8b8a97dfc9c83da39d7a32b9b
abdallhfattah <abdallh.fattah33@gmail.com>
2024-09-15 13:41:15 +0300

    solved

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""module that solves famous N Queen problem"""
+import sys
+from typing import List, Any
+
+
+def solveQueens(n: int) -> List[List[Any]]:
+    """function to solve N queen problem"""
+    col = set()
+    posDiag = set()  # (r + c)
+    negDiag = set()  # (r - c)
+    solutions = []
+    board = [[0] * n for i in range(n)]
+
+    def backtrack(r: int):
+        if r == n:
+            copy = [coll for row in board for coll in row if coll != 0]
+            solutions.append(copy)
+            return
+
+        for c in range(n):
+            if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                continue
+            else:
+                # updating
+                board[r][c] = [r, c]
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                col.add(c)
+                # backtracking
+                backtrack(r + 1)
+                # clean up
+                board[r][c] = 0
+                posDiag.discard(r + c)
+                negDiag.discard(r - c)
+                col.discard(c)
+
+    backtrack(0)
+
+    return solutions
+
+
+if __name__ == "__main__":
+    # board dimensions
+    try:
+        dimensions = int(sys.argv[1])
+    except TypeError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if dimensions <= 3:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    print(solveQueens(dimensions))
