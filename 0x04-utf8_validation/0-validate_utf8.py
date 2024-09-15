8696ac0f9119f9d8ef5533acea8f1af03bb72d05
abdallhfattah <abdallh.fattah33@gmail.com>
2024-09-15 19:02:41 +0300

    validate utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,11 +1,19 @@
-#!/usr/bin/python3
-
-""" module to validate utf-8"""
-
-
 def validUTF8(data) -> bool:
-    for element in data:
-        if element >= 256:
-            return False
+    num_bytes = 0
+    for byte in data:
+        byte = byte & 0xFF
+        if num_bytes == 0:
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif byte >> 7:
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
 
-    return True
+    return num_bytes == 0
