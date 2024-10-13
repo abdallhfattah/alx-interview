657ab84b9929404de7fdf036beb370fb9df73a13
abdallhfattah <abdallh.fattah33@gmail.com>
2024-10-13 18:53:57 +0300

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,16 +7,18 @@ def makeChange(coins, total):
     """
     make change (greedy solution probably will not work)
     """
-    sorted(coins)
     if total <= 0:
         return 0
+
+    coins.sort(reverse=True)
+
     number_of_coins = 0
-    for coin in reversed(coins):
+    for coin in coins:
+        if total == 0:
+            break
         if total % coin == 0:
             what_i_will_take = total // coin
             number_of_coins += number_of_coins
             total -= what_i_will_take * coin
-    if total != 0:
-        return -1
-    else:
-        return number_of_coins
+
+    return -1 if total else number_of_coins
