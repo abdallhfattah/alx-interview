8e0fb3e9e48ac8b055ccf6283b659a1fdc40feae
abdallhfattah <abdallh.fattah33@gmail.com>
2024-10-13 18:56:50 +0300

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,18 +11,18 @@ def makeChange(coins, total):
         return 0
 
     coins.sort(reverse=True)
-
+    print(coins)
     number_of_coins = 0
     for coin in coins:
         if total == 0:
             break
-        if total % coin == 0:
-            what_i_will_take = total // coin
-            number_of_coins += what_i_will_take
-            total -= what_i_will_take * coin
+        what_i_will_take = total // coin
+        number_of_coins += what_i_will_take
+        print(f"what i will take for the {coin} will be {what_i_will_take}")
+        total -= what_i_will_take * coin
 
     return -1 if total else number_of_coins
 
 print(makeChange([1, 2, 25], 37))
 
-print(makeChange([1256, 54, 48, 16, 102], 1453))
+# print(makeChange([1256, 54, 48, 16, 102], 1453))
