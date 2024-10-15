1f292590898ddf9011e2f0262be7086a943932f9
abdallhfattah <abdallh.fattah33@gmail.com>
2024-10-15 14:46:01 +0300

    0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,8 +1,24 @@
+#!/usr/bin/python3
+"""
+module to find a winner of prime game
+"""
+
+
 class PrimeGame:
+    """
+    prime game controller
+    """
+
     def __init__(self):
+        """
+        init method
+        """
         self.prime_numbers = []
 
     def sieve(self, n):
+        """
+        Sieve of Eratosthenes
+        """
         prime = [True] * (n + 1)
         prime[0] = prime[1] = False
 
@@ -11,15 +27,17 @@ class PrimeGame:
                 for multiple in range(p * p, n + 1, p):
                     prime[multiple] = False
 
-        self.prime_numbers = [i for i, is_prime in enumerate(prime) if is_prime]
+        self.prime_numbers = [i for i, is_pri in enumerate(prime) if is_pri]
 
     def lower_bound(self, target):
-
+        """
+        custom lower bound method
+        """
         if self.prime_numbers[-1] < target:
             return len(self.prime_numbers) - 1
 
         if self.prime_numbers[0] > target:
-            return 1 # indicate that ben won
+            return 1  # indicate that ben won
 
         left, right = 0, len(self.prime_numbers)
         while left < right:
@@ -32,12 +50,17 @@ class PrimeGame:
         return left
 
     def check_winner_for_this_round(self, number):
+        """
+        checking the winner for some round based on the given number
+        """
         index = self.lower_bound(number)
         # print("index : " , index)
         return -1 if index % 2 else 1
 
     def is_winner_helper(self, x, nums):
-
+        """
+        class implementation of the outer function
+        """
         if x <= 0 or nums is None or x != len(nums):
             # print("i actaully stoped here")
             return None
@@ -62,10 +85,8 @@ class PrimeGame:
 
 
 def is_winner(rounds, nums):
+    """
+    is winner function
+    """
     prime = PrimeGame()
     return prime.is_winner_helper(rounds, nums)
-
-
-if __name__ == '__main__':
-    # print(is_winner(2, [1, 2]))
-    print(is_winner(3, [4, 4, 4]))
